0b2d038b33b7d8ccce5f3df6da93ee310e1d890c
evanxu <evanxu@didiglobal.com>
2022-11-02 00:04:26 +0800

    add

--- test/index3/article_comment_test.go
+++ test/index3/article_comment_test.go
@@ -2,7 +2,9 @@ package index3
 
 import (
 	"encoding/json"
+	"fmt"
 	. "gopkg.in/check.v1"
+	"otherpay-test/client"
 	"otherpay-test/common"
 	"time"
 )
@@ -48,6 +50,7 @@ func (s *ArticleComment) TestArticleCommentCase00(goCheck *C) {
 	goCheck.Assert(err, IsNil)
 	goCheck.Assert(resp.Code, Equals, uint32(0))
 
+	//评论文章
 	reqArticleComment := ArticleCommentReq{
 		ArticleID:      "UU5yS0VH_4DJpGqVzOtXCaEm2dYhtVpifO_TBqx5P-M",
 		SourceType:     "mirror",
@@ -66,4 +69,43 @@ func (s *ArticleComment) TestArticleCommentCase00(goCheck *C) {
 	err = json.Unmarshal(respStr, &respArticleComment)
 	goCheck.Assert(err, IsNil)
 	goCheck.Assert(respArticleComment.Code, Equals, uint32(0))
+	goCheck.Assert(respArticleComment.Data.(map[string]string)["id"], Not(Equals), "")
+	commentID := respArticleComment.Data.(map[string]string)["id"]
+	//parientComment != "", 评论回复
+	reqArticleComment1 := ArticleCommentReq {
+		ArticleID:      "UU5yS0VH_4DJpGqVzOtXCaEm2dYhtVpifO_TBqx5P-M",
+		SourceType:     "mirror",
+		Content:        "test comment 456 after 123",
+		ParientComment: commentID,
+		CheckInfo:      CheckInfo{
+			Address: addr,
+			Msg:     msg,
+			Sign:    sign,
+		},
+	}
+	respStr1, err1 := common.DoPost(ArticleCommentUrl, common.ConvToJSON(reqArticleComment1))
+	goCheck.Assert(err1, IsNil)
+	common.PrintInfo("ArticleCommentUrl_resp1: %v", string(respStr1))
+	err = json.Unmarshal(respStr1, &respArticleComment)
+	goCheck.Assert(err, IsNil)
+	commentID1 := respArticleComment.Data.(map[string]string)["id"]
+	sql := fmt.Sprintf("select article_id, content, parent, from_user_id, to_user_id from comment where id = \"%s\"", commentID1)
+	rows, err := client.MysqlClientIndex3().Query(sql)
+	goCheck.Assert(err, IsNil)
+
+	var articleID string
+	var content string
+	var parent string
+	var fromUserID string
+	var toUserID string
+	for rows.Next() {
+		rows.Scan(&articleID, &content, &parent, &fromUserID, &toUserID)
+	}
+	goCheck.Assert(articleID, Equals, "UU5yS0VH_4DJpGqVzOtXCaEm2dYhtVpifO_TBqx5P-M")
+	goCheck.Assert(content, Equals, "test comment 456 after 123")
+	goCheck.Assert(parent, Equals, commentID)
+	goCheck.Assert(fromUserID, Equals, addr)
+	goCheck.Assert(toUserID, Equals, addr)
+
+
 }
